05d7c14dc062be7de4a2588936002221fc8dc6a8
ezequielastrada <eastrada.in@gmail.com>
2024-12-04 22:03:50 -0300

    comentarios

--- registro_temperatura/monitoreo.js
+++ registro_temperatura/monitoreo.js
@@ -3,19 +3,21 @@ const {monitorTemperature} = require('./logica')
 
 const startMonitoring = (sampleCount, intervalMs = 1000) => {
 
+    // Comprobacion de argumentos correctos
     if (typeof sampleCount !== 'number' || typeof intervalMs !== 'number') {
         console.error('❌❌❌ ERROR: SOLO SE ADMITEN NUMEROS COMO ARGUMENTOS ❌❌❌')
         return
     }
-
     if (sampleCount <= 0) {
         console.error("❌ ERROR: La cantidad de muestras debe ser un número mayor a 0.");
         return;
     }
 
-    let iteration = 0
+    // Inicio de monitoreo
     myEmisor.emit("start")
 
+    // Monitoreo de temperaturas mediante setInterval()
+    let iteration = 0
     const intervalId = setInterval(()=>{
         if (iteration == sampleCount) {
             clearInterval(intervalId)
